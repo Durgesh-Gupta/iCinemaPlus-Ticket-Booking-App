61ef0be8f8a94b94646f6de6495d3e71d8b2d522
dgupta <DurgeshG@123>
2022-01-30 19:41:09 +0530

    make it better

--- Backend/server.js
+++ Backend/server.js
@@ -35,9 +35,14 @@ app.get('/', (req, res) => {
 })
 
 // For Heroku
-if(process.env.Node_ENV==="production"){
-  app.use(express.static("client/build"))
-
+// server static assets if in production
+if(process.env.NODE_ENV === 'production'){    
+  app.use(express.static('client/build'))  // set static folder 
+  //returning frontend for any route other than api 
+  app.get('*',(req,res)=>{     
+      res.sendFile (path.resolve(__dirname,'client','build',         
+                    'index.html' ));    
+  });
 }
 
 
